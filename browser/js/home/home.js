de0fa365e082abef77ec234f9158697198271007
EmilySanford <emilyannsanford@gmail.com>
2015-09-25 00:19:58 -0400

    buddy factory

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,21 @@
 app.config(function ($stateProvider) {
   $stateProvider.state('home', {
     url: '/',
-    templateUrl: 'js/home/home.html'
+    templateUrl: 'js/home/home.html',
+    controller: 'MainController',
+    resolve: {
+    	theBuddy: function(BuddyFactory){
+    		return BuddyFactory.getBuddies();
+    	}
+    }
   });
 });
+
+
+app.controller('MainController', function($scope, BuddyFactory, theBuddy, $state){
+	$scope.buddies = theBuddy;
+	
+})
+
+
+
